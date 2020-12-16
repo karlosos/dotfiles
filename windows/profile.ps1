3a5181f82e86be239ef79cad0257ec7e3c76ea5c
Karol Dzialowski <karlososhd@gmail.com>
2020-12-16 13:31:10 +0100

    Add open and here aliases to windows profile

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -1,6 +1,14 @@
 Function CDDEV {Set-Location -Path C:\Dev}
+
+Function OPEN-EXPLORER {
+    explorer.exe .
+}
+
 Set-Alias -Name d -Value CDDEV
 Set-Alias -Name dev -Value CDDEV
 Set-Alias -Name vim -Value nvim-qt
+Set-Alias -Name here -Value OPEN-EXPLORER 
+Set-Alias -Name open -Value explorer.exe
+Set-Alias -Name o -Value explorer.exe
 
 # Import-Module posh-git
